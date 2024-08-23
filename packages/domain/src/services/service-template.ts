86cb86fed0ef1d261d72b0c642c61e84d77145f6
Nicolas Filippelli <nicolasfilippelli22@gmail.com>
2024-08-23 10:58:52 -0300

    Changed service params names

--- packages/domain/src/services/service-template.ts
+++ packages/domain/src/services/service-template.ts
@@ -1,7 +1,7 @@
 export interface ServiceTemplate<T> {
   getAll: () => Promise<T[]>;
   getOne: (id: string) => Promise<T | null>;
-  add: (newItem: T) => Promise<void>;
+  add: (item: T) => Promise<void>;
   remove: (id: string) => Promise<void>;
-  update: (updatedItem: T) => Promise<void>;
+  update: (item: T) => Promise<void>;
 }
