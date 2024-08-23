a9ab63dd51d33be0a0fd19f522ed48f22770af15
Nicolas Filippelli <nicolasfilippelli22@gmail.com>
2024-08-23 11:04:49 -0300

    Added new property to experience and new EducationService

--- packages/domain/src/entities/experience.ts
+++ packages/domain/src/entities/experience.ts
@@ -5,4 +5,5 @@ export interface Experience {
   startDate: string;
   finishDate?: string;
   description: string[];
+  url?: string;
 }
---
+++ packages/domain/src/services/education-service.ts
@@ -0,0 +1,4 @@
+import { Experience } from "../entities/experience";
+import { ServiceTemplate } from "./service-template";
+
+export interface EducationService extends ServiceTemplate<Experience> {}
