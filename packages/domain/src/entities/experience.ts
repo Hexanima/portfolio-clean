1297917d6ce1ec89cb508aa42c8dfabf6876697c
Hexanima <nicolasfilippelli22@gmail.com>
2024-08-23 09:47:31 -0300

    Added some entities

---
+++ packages/domain/src/entities/experience.ts
@@ -0,0 +1,7 @@
+export interface Experience {
+  id: string;
+  startDate: string;
+  finishDate?: string;
+  title: string
+  
+}
---
+++ packages/domain/src/entities/language.ts
@@ -0,0 +1,6 @@
+export interface Language {
+  id: string;
+  title: string;
+  grade: string;
+  certificateLink: string;
+}
